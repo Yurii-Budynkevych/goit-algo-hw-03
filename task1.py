fcbd336c4316ab8c4b5514cfbf008746dd78bedc
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2024-01-25 21:24:42 +0200

    task1

--- task1.py
+++ task1.py
@@ -1,5 +1,14 @@
-import datetime
+import datetime as dt
+
+NOW = dt.datetime.now().date()
+input = input('enter your date (YYYY-MM-DD): ')
 
 def get_days_from_today(date):
-    input('enter your date: ')
-    return(date)
+    try:
+        paresd_date = dt.datetime.strptime(date, '%Y-%m-%d').date()
+        delta = NOW - paresd_date
+        print(delta.days, 'days passed')
+    except Exception as error:
+        print('Error occured:', error, sep=' ')    
+
+get_days_from_today(input)    
