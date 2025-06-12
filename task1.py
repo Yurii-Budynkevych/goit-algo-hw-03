d40a40f7d5f2bcb49c2c787a78c7d5470ae16a0f
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2025-06-12 23:11:07 +0200

    task1 fix

--- task1.py
+++ task1.py
@@ -1,14 +1,16 @@
 import datetime as dt
 
-NOW = dt.datetime.now().date()
-input = input('enter your date (YYYY-MM-DD): ')
-
 def get_days_from_today(date):
+    NOW = dt.datetime.now().date()
+
     try:
         paresd_date = dt.datetime.strptime(date, '%Y-%m-%d').date()
         delta = NOW - paresd_date
+        if delta.days < 0:
+            return "date is in the future" 
         return delta.days
     except Exception as error:
         return error
 
-print(get_days_from_today(input)) 
+user_input = input('enter your date (YYYY-MM-DD): ')
+print(get_days_from_today(user_input)) 
