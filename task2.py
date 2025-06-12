a6c702c95dbf809bd002d236f7df2c078c817905
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2025-06-12 23:23:27 +0200

    task2 fix

--- task2.py
+++ task2.py
@@ -3,10 +3,10 @@ import random
 def get_numbers_ticket(min, max, quantity=1):
     try:
         if min < 1 or max > 1000:
-            return []  
-        lotery = random.sample(range(min, max+1), quantity)
+            return []
+        lotery = sorted(random.sample(range(min, max + 1), quantity))
         return lotery
     except Exception as error:
-        return error 
+        return []
 
 print(get_numbers_ticket(1, 1000, 6))
